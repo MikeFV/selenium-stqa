88fc421ed587ef3bf655c7caae737a5a772d26a8
Mikhail <a@a.com>
2020-09-24 20:51:47 +0300

    .

--- src/test/java/CheckEditCountryPageExternalLinks.java
+++ src/test/java/CheckEditCountryPageExternalLinks.java
@@ -1,6 +1,4 @@
-import Base.BaseHelper;
 import Base.BaseManager;
-import PageObjects.Pages;
 import PageSteps.AdminLoginPageSteps;
 import PageSteps.AdminMainPageSteps;
 import PageSteps.CountriesPagesSteps;
@@ -13,8 +11,6 @@ import org.openqa.selenium.WebDriver;
 public class CheckEditCountryPageExternalLinks {
     private BaseManager bm;
     private WebDriver wd;
-    private BaseHelper baseHelper;
-    private Pages pages;
     private AdminLoginPageSteps adminLoginPageSteps;
     private AdminMainPageSteps adminMainPageSteps;
     private CountriesPagesSteps countriesPagesSteps;
@@ -25,8 +21,6 @@ public class CheckEditCountryPageExternalLinks {
     public void setUp() throws Exception {
         bm = BaseManager.getInstance();
         wd = bm.getWebDriverManager().getDriver();
-        baseHelper = bm.getBaseHelper();
-        pages = Pages.getInstance();
         adminLoginPageSteps = new AdminLoginPageSteps();
         adminMainPageSteps = new AdminMainPageSteps();
         countriesPagesSteps = new CountriesPagesSteps();
